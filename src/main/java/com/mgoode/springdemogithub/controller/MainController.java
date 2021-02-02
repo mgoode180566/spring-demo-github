61c45cf3d8707a76e3044a8c5ed203b2e356af1c
mgoode180566 <michaelgoode66@googlemail.com>
2021-02-02 23:51:05 +0000

    added message endpoint

--- src/main/java/com/mgoode/springdemogithub/controller/MainController.java
+++ src/main/java/com/mgoode/springdemogithub/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.mgoode.springdemogithub.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class MainController {
     @GetMapping("/")
     public String getHome() {
         return "this is home";
+    }
+
+    @GetMapping("/message")
+    public String getMessage(@RequestParam("msg") String msg) {
+        return "This is the message " + msg;
     }
 }
